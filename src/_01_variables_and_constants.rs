f3e23ad094d54a1bdc3c8b42829652666e5be05f
Gustavo Becelli <gustavobecelli@gmail.com>
2022-10-15 20:16:10 -0300

    feat: basic exercises

---
+++ src/_01_variables_and_constants.rs
@@ -0,0 +1,9 @@
+pub fn example() {
+    const PI: f32 = 3.141592;
+
+    println!("The value of PI is: {}", PI);
+
+    let x = 22.0 / 7.0;
+    println!("The value of x is: {}", x);
+    // const pi_2: f32 = x;
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#[allow(dead_code)]
+mod _01_variables_and_constants;
+mod _02_data_types;
+mod _03_functions;
+mod _04_composed_types;
+mod _05_structs_enums;
+mod _06_flow_control;
+mod _07_scopes;
+mod _08_pattern;
+mod _slices_and_references;
+fn main() {
+    // _01_variables_and_constants::example();
+    // _03_functions::example();
+    // _02_data_types::example();
+    // _04_composed_types::example();
+    // _05_structs_enums::example();
+    // _06_flow_control::example();
+    // _07_scopes::example();
+    // _08_pattern::example();
+    _slices_and_references::example();
+}
